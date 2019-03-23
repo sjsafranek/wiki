8561d8dd871670412e7e259bbd0daa4b1958fc32
Stefan Safranek <stefan@stefan>
2019-03-22 17:54:35 -0700

    enabled for gunicorn

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-import binascii
-import hashlib
+
 import os
 import re
 import markdown
-import json
 import argparse
 
 from functools import wraps
 
 from flask import (Flask, render_template, flash, redirect, url_for, request,
                    abort)
+
+from flask import current_app
 
 from flask_wtf import FlaskForm
 from wtforms import (BooleanField, TextField, TextAreaField, PasswordField)
@@ -19,16 +19,49 @@ from flask_login import (LoginManager, login_required, current_user,
                              login_user, logout_user)
 
 
+from flask import Blueprint
+
+
+from wiki import Wiki
+from users import UserManager
+
+__VERSION__ = '0.0.1'
+
 
 """
     Application Setup
     ~~~~~~~~~
 """
+
+# parser = argparse.ArgumentParser(description='FlaskWiki')
+# parser.add_argument('-p', '--port', type=int, default=8000, help='Port')
+# parser.add_argument('-dir', '--directory', type=str, default='content', help='Content directory')
+# parser.add_argument('-d', '--debug', action='store_true', help='Debug mode')
+# parser.add_argument('-v', '--verbose', action='store_true', help='Verbose mode')
+# parser.add_argument('-V', '--version', action='store_true', help='Print version and exit')
+# parser.add_argument('-a', '--authentication_method', type=str, default='cleartext', help='Default authentication method')
+# options = parser.parse_args()
+
+# if options.version:
+#     print('FlaskWiki-{0}'.format(__VERSION__))
+#     exit()
+
 app = Flask(__name__)
+
+# mux = Blueprint('default', __name__)
+# app.register_blueprint(mux)
+
+# set default config items
 app.config['DEBUG'] = True
 app.config['CONTENT_DIR'] = 'content'
+# app.config['DEBUG'] = options.debug
+# app.config['CONTENT_DIR'] = options.directory
 app.config['TITLE'] = 'wiki'
-app.config['DEFAULT_AUTHENTICATION_METHOD'] = 'cleartext'
+# app.config['AUTHENTICATION_METHOD'] = options.authentication_method
+app.config['AUTHENTICATION_METHOD'] = 'cleartext'
+app.config['SEARCH_IGNORE_CASE'] = True
+
+
 try:
     app.config.from_pyfile(
         os.path.join(app.config.get('CONTENT_DIR'), 'config.py')
@@ -37,432 +70,19 @@ except IOError:
     print ("Startup Failure: You need to place a "
            "config.py in your content directory.")
 
+
+wiki = Wiki(app.config.get('CONTENT_DIR'))
+users = UserManager(app.config.get('CONTENT_DIR'))
+
+users.add_user('admin', 'dev', active=True, roles=[], authentication_method=app.config.get('AUTHENTICATION_METHOD'))
+
+
 loginmanager = LoginManager()
 loginmanager.init_app(app)
 loginmanager.login_view = 'user_login'
 
 
 
-
-"""
-    Wiki classes
-    ~~~~~~~~~~~~
-"""
-
-
-class Processors(object):
-    """This class is collection of processors for various content items.
-    """
-    def __init__(self, content=""):
-        """Initialization function.  Runs Processors().pre() on content.
-
-        Args:
-            None
-
-        Kwargs:
-            content (str): Preprocessed content directly from the file or
-            textarea.
-        """
-        self.content = self.pre(content)
-
-    def wikilink(self, html):
-        """Processes Wikilink syntax "[[Link]]" within content body.  This is
-        intended to be run after the content has been processed by Markdown.
-
-        Args:
-            html (str): Post-processed HTML output from Markdown
-
-        Kwargs:
-            None
-
-        Syntax: This accepts Wikilink syntax in the form of [[WikiLink]] or
-        [[url/location|LinkName]].  Everything is referenced from the base
-        location "/", therefore sub-pages need to use the
-        [[page/subpage|Subpage]].
-        """
-        link = r"((?<!\<code\>)\[\[([^<].+?) \s*([|] \s* (.+?) \s*)?]])"
-        compLink = re.compile(link, re.X | re.U)
-        for i in compLink.findall(html):
-            title = [i[-1] if i[-1] else i[1]][0]
-            url = self.clean_url(i[1])
-            formattedLink = u"<a href='{0}'>{1}</a>".format(url_for('display', url=url), title)
-            html = re.sub(compLink, formattedLink, html, count=1)
-        return html
-
-    def clean_url(self, url):
-        """Cleans the url and corrects various errors.  Removes multiple spaces
-        and all leading and trailing spaces.  Changes spaces to underscores and
-        makes all characters lowercase.  Also takes care of Windows style
-        folders use.
-
-        Args:
-            url (str): URL link
-
-        Kwargs:
-            None
-        """
-        pageStub = re.sub('[ ]{2,}', ' ', url).strip()
-        pageStub = pageStub.lower().replace(' ', '_')
-        pageStub = pageStub.replace('\\\\', '/').replace('\\', '/')
-        return pageStub
-
-    def pre(self, content):
-        """Content preprocessor.  This currently does nothing.
-
-        Args:
-            content (str): Preprocessed content directly from the file or
-            textarea.
-
-        Kwargs:
-            None
-        """
-        return content
-
-    def post(self, html):
-        """Content post-processor.
-
-        Args:
-            html (str): Post-processed HTML output from Markdown
-
-        Kwargs:
-            None
-        """
-        return self.wikilink(html)
-
-    def out(self):
-        """Final content output.  Processes the Markdown, post-processes, and
-        Meta data.
-        """
-        # md = markdown.Markdown(['codehilite', 'fenced_code', 'meta', 'tables'])
-        md = markdown.Markdown(extensions=['codehilite', 'fenced_code', 'meta', 'tables'])
-        html = md.convert(self.content)
-        phtml = self.post(html)
-        body = self.content.split('\n\n', 1)[1]
-        meta = md.Meta
-        return phtml, body, meta
-
-
-class Page(object):
-    def __init__(self, path, url, new=False):
-        self.path = path
-        self.url = url
-        self._meta = {}
-        if not new:
-            self.load()
-            self.render()
-
-    def load(self):
-        with open(self.path, 'rU') as f:
-            self.content = f.read() #.decode('utf-8')
-
-    def render(self):
-        processed = Processors(self.content)
-        self._html, self.body, self._meta = processed.out()
-
-    def save(self, update=True):
-        folder = os.path.dirname(self.path)
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        with open(self.path, 'w') as f:
-            for key, value in self._meta.items():
-                line = '%s: %s\n' % (key, value)
-                f.write(line)
-            f.write('\n')
-            f.write(self.body.replace('\r\n', '\n'))
-        if update:
-            self.load()
-            self.render()
-
-    @property
-    def meta(self):
-        return self._meta
-
-    def __getitem__(self, name):
-        item = self._meta[name]
-        if len(item) == 1:
-            return item[0]
-        print(item)
-        return item
-
-    def __setitem__(self, name, value):
-        self._meta[name] = value
-
-    @property
-    def html(self):
-        return self._html
-
-    def __html__(self):
-        return self.html
-
-    @property
-    def title(self):
-        try:
-            return self['title']
-        except KeyError:
-            return self.url
-
-    @title.setter
-    def title(self, value):
-        self['title'] = value
-
-    @property
-    def tags(self):
-        try:
-            return self['tags']
-        except KeyError:
-            return ""
-
-    @tags.setter
-    def tags(self, value):
-        self['tags'] = value
-
-
-class Wiki(object):
-    def __init__(self, root):
-        self.root = root
-
-    def path(self, url):
-        return os.path.join(self.root, url + '.md')
-
-    def exists(self, url):
-        path = self.path(url)
-        return os.path.exists(path)
-
-    def get(self, url):
-        path = os.path.join(self.root, url + '.md')
-        if self.exists(url):
-            return Page(path, url)
-        return None
-
-    def get_or_404(self, url):
-        page = self.get(url)
-        if page:
-            return page
-        abort(404)
-
-    def get_bare(self, url):
-        path = self.path(url)
-        if self.exists(url):
-            return False
-        return Page(path, url, new=True)
-
-    def move(self, url, newurl):
-        os.rename(
-            os.path.join(self.root, url) + '.md',
-            os.path.join(self.root, newurl) + '.md'
-        )
-
-    def delete(self, url):
-        path = self.path(url)
-        if not self.exists(url):
-            return False
-        print(path)
-        os.remove(path)
-        return True
-
-    def index(self, attr=None):
-        def _walk(directory, path_prefix=()):
-            for name in os.listdir(directory):
-                fullname = os.path.join(directory, name)
-                if os.path.isdir(fullname):
-                    _walk(fullname, path_prefix + (name,))
-                elif name.endswith('.md'):
-                    if not path_prefix:
-                        url = name[:-3]
-                    else:
-                        url = os.path.join('/'.join(path_prefix), name[:-3])
-                    if attr:
-                        pages[getattr(page, attr)] = page  # TODO: looks like bug, but doesn't appear to be used
-                    else:
-                        pages.append(Page(fullname, url.replace('\\', '/')))
-        if attr:
-            pages = {}
-        else:
-            pages = []
-        _walk(self.root)
-        if not attr:
-            return sorted(pages, key=lambda x: x.title.lower())
-        return pages
-
-    def get_by_title(self, title):
-        pages = self.index(attr='title')
-        return pages.get(title)
-
-    def get_tags(self):
-        pages = self.index()
-        tags = {}
-        for page in pages:
-            pagetags = page.tags.split(',')
-            for tag in pagetags:
-                tag = tag.strip()
-                if tag == '':
-                    continue
-                elif tags.get(tag):
-                    tags[tag].append(page)
-                else:
-                    tags[tag] = [page]
-        return tags
-
-    def index_by_tag(self, tag):
-        pages = self.index()
-        tagged = []
-        for page in pages:
-            if tag in page.tags:
-                tagged.append(page)
-        return sorted(tagged, key=lambda x: x.title.lower())
-
-    def search(self, term, ignore_case=True, attrs=['title', 'tags', 'body']):
-        pages = self.index()
-        regex = re.compile(term, re.IGNORECASE if ignore_case else 0)
-        matched = []
-        for page in pages:
-            for attr in attrs:
-                if regex.search(getattr(page, attr)):
-                    matched.append(page)
-                    break
-        return matched
-
-
-"""
-    User classes & helpers
-    ~~~~~~~~~~~~~~~~~~~~~~
-"""
-
-
-class UserManager(object):
-    """A very simple user Manager, that saves it's data as json."""
-    def __init__(self, path):
-        self.file = os.path.join(path, 'users.json')
-
-    def read(self):
-        if not os.path.exists(self.file):
-            return {}
-        with open(self.file) as f:
-            data = json.loads(f.read())
-        return data
-
-    def write(self, data):
-        with open(self.file, 'w') as f:
-            f.write(json.dumps(data, indent=2))
-
-    def add_user(self, name, password,
-                 active=True, roles=[], authentication_method=None):
-        users = self.read()
-        if users.get(name):
-            return False
-        if authentication_method is None:
-            authentication_method = get_default_authentication_method()
-        new_user = {
-            'active': active,
-            'roles': roles,
-            'authentication_method': authentication_method,
-            'authenticated': False
-        }
-        # Currently we have only two authentication_methods: cleartext and
-        # hash. If we get more authentication_methods, we will need to go to a
-        # strategy object pattern that operates on User.data.
-        if authentication_method == 'hash':
-            new_user['hash'] = make_salted_hash(password)
-        elif authentication_method == 'cleartext':
-            new_user['password'] = password
-        else:
-            raise NotImplementedError(authentication_method)
-        users[name] = new_user
-        self.write(users)
-        userdata = users.get(name)
-        return User(self, name, userdata)
-
-    def get_user(self, name):
-        users = self.read()
-        userdata = users.get(name)
-        if not userdata:
-            return None
-        return User(self, name, userdata)
-
-    def delete_user(self, name):
-        users = self.read()
-        if not users.pop(name, False):
-            return False
-        self.write(users)
-        return True
-
-    def update(self, name, userdata):
-        data = self.read()
-        data[name] = userdata
-        self.write(data)
-
-
-class User(object):
-    def __init__(self, manager, name, data):
-        self.manager = manager
-        self.name = name
-        self.data = data
-
-    def get(self, option):
-        return self.data.get(option)
-
-    def set(self, option, value):
-        self.data[option] = value
-        self.save()
-
-    def save(self):
-        self.manager.update(self.name, self.data)
-
-    def is_authenticated(self):
-        return self.data.get('authenticated')
-
-    def is_active(self):
-        return self.data.get('active')
-
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return self.name
-
-    def check_password(self, password):
-        """Return True, return False, or raise NotImplementedError if the
-        authentication_method is missing or unknown."""
-        authentication_method = self.data.get('authentication_method', None)
-        if authentication_method is None:
-            authentication_method = get_default_authentication_method()
-        # See comment in UserManager.add_user about authentication_method.
-        if authentication_method == 'hash':
-            result = check_hashed_password(password, self.get('hash'))
-        elif authentication_method == 'cleartext':
-            result = (self.get('password') == password)
-        else:
-            raise NotImplementedError(authentication_method)
-        return result
-
-
-def get_default_authentication_method():
-    return app.config.get('DEFAULT_AUTHENTICATION_METHOD', 'cleartext')
-
-
-def make_salted_hash(password, salt=None):
-    if not salt:
-        salt = os.urandom(64)
-    password = password.encode('utf-8')
-    d = hashlib.sha512()
-    d.update(salt[:32])
-    d.update(password)
-    d.update(salt[32:])
-    return binascii.hexlify(salt).decode('utf-8') + d.hexdigest()
-
-
-def check_hashed_password(password, salted_hash):
-    salt = binascii.unhexlify(salted_hash[:128])
-    return make_salted_hash(password, salt) == salted_hash
-
-
-def protect(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        if app.config.get('PRIVATE') and not current_user.is_authenticated:
-            return loginmanager.unauthorized()
-        return f(*args, **kwargs)
-    return wrapper
 
 
 """
@@ -484,7 +104,7 @@ class URLForm(FlaskForm):
 
 class SearchForm(FlaskForm):
     term = TextField('', [InputRequired()])
-    ignore_case = BooleanField(description='Ignore Case', default=app.config.get('DEFAULT_SEARCH_IGNORE_CASE', True))
+    ignore_case = BooleanField(description='Ignore Case', default=app.config.get('SEARCH_IGNORE_CASE'))
 
 
 class EditorForm(FlaskForm):
@@ -506,17 +126,13 @@ class LoginForm(FlaskForm):
         user = users.get_user(form.name.data)
         if not user:
             return
-        if not user.check_password(field.data):
+        if not user.check_password(field.data, current_app.config.get('AUTHENTICATION_METHOD')):
             raise ValidationError('Username and password do not match.')
 
 class CreateUserForm(FlaskForm):
     name = TextField('', [InputRequired()])
     password = PasswordField('', [InputRequired()])
 
-
-wiki = Wiki(app.config.get('CONTENT_DIR'))
-
-users = UserManager(app.config.get('CONTENT_DIR'))
 
 
 @loginmanager.user_loader
@@ -525,11 +141,23 @@ def load_user(name):
 
 
 
+def protect(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if app.config.get('PRIVATE') and not current_user.is_authenticated:
+            return loginmanager.unauthorized()
+        return f(*args, **kwargs)
+    return wrapper
+
+
+
 """
     Routes
     ~~~~~~
 """
 
+# mux = Blueprint('mux', __name__, url_prefix='/wiki')
+# mux = Blueprint('mux', __name__)
 
 @app.route('/')
 @protect
@@ -691,14 +319,10 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='FlaskWiki')
-    parser.add_argument('--port', type=int, default=8000, help='Port')
-    parser.add_argument('--dir', type=str, default='content', help='Content directory')
-    args = parser.parse_args()
 
-    users.add_user('admin', 'dev')
-
-    app.run(
-        port = args.port
-    )
+# if __name__ == '__main__':
+#     # app.register_blueprint(mux)
+#     app.run(
+#         host = '0.0.0.0',
+#         port = options.port
+#     )
